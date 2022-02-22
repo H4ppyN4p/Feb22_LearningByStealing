1128f9d38165376b374401625b5443d798622d83
H4ppyN4p <tobi5703@stud.kea.dk>
2022-02-22 16:38:34 +0100

    Finished assignment; 1 menu, 2 articles Everything responsive using % and vw sizes

---
+++ src/main/java/com/example/feb22_learnbystealing/Feb22LearnByStealingApplication.java
@@ -0,0 +1,14 @@
+package com.example.feb22_learnbystealing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Feb22LearnByStealingApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(Feb22LearnByStealingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/feb22_learnbystealing/Feb22LearnByStealingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.feb22_learnbystealing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Feb22LearnByStealingApplicationTests
+{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
